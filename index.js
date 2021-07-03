e0407212e08de16b69908e7f2718f84f2198dc20
guynas <guynas@ya.ru>
2021-07-03 15:07:54 +0300

    hexlet NPX lesson

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ console.log("Hello, Hexlet!");
 import _ from "lodash";
 _.intersection([2, 1], [2, 3]);
 _.capitalize("hello");
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
